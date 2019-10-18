e12732d55497b67f9e16136d6c307fd5a748c9cf
PyDev <pydeveloper455@gmail.com>
2019-10-18 18:33:42 +0530

    added server setting files

---
+++ settings.py
@@ -0,0 +1,10 @@
+########################################
+# Set various settings for app here   #
+#                                     #
+########################################
+
+# set host name
+SERVER_HOST = '0.0.0.0'
+
+# set port name
+SERVER_PORT = 9999
